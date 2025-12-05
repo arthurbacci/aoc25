42f2431cf3009c39d5ba537969b16a88fa34920e
Arthur Bacci <arthurbacci@protonmail.com>
2025-12-05 18:39:58 -0300

    feat: day5 part 2 and better part 1

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -17,25 +17,71 @@ peg::parser! {
     }
 }
 
+
+fn search_id(id: u64, ranges: &[(u64, u64)]) -> Option<(u64, u64)> {
+    let in_this_one = |i: usize| ranges[i].0 <= id && ranges[i].1 >= id;
+    match ranges.len() {
+        0 => None,
+        1 => (in_this_one(0)).then_some(ranges[0]),
+        n => {
+            (in_this_one(n / 2).then_some(ranges[n / 2]))
+                .or(
+                    if ranges[n / 2].0 > id {
+                        search_id(id, &ranges[0..(n / 2)])
+                    } else if n / 2 + 1 < ranges.len() {
+                        search_id(id, &ranges[(n / 2 + 1)..])
+                    } else {
+                        None
+                    }
+                )
+        }
+    }
+}
+
+
+fn unintersect(ranges: &mut Vec<(u64, u64)>) {
+    let mut i = 0;
+    loop {
+        if i + 1 >= ranges.len() {
+            break;
+        }
+        if ranges[i + 1].0 <= ranges[i].1 {
+            ranges[i].1 = ranges[i].1.max(ranges[i + 1].1);
+            ranges.remove(i + 1);
+        } else {
+            i += 1;
+        }
+    }
+}
+
 fn main() {
     let mut f = File::open("day5.txt").unwrap();
     let mut data = String::new();
     f.read_to_string(&mut data).unwrap();
 
-    let (ranges, ids) = list_parser::ranges_and_ids(&data).unwrap();
+    let (mut ranges, ids) = list_parser::ranges_and_ids(&data).unwrap();
 
-    let mut total = 0;
+    ranges.sort_unstable_by(|(a, _), (b, _)| a.cmp(b));
 
+    println!("{}", ranges.len());
+
+    unintersect(&mut ranges);
+
+    println!("{}", ranges.len());
+
+    let mut part1 = 0;
+    let mut part2 = 0;
+    
     for id in ids {
-        let found = ranges
-            .iter()
-            .find(|&&(a, b)| id >= a && id <= b)
-            .is_some();
-
-        if found {
-            total += 1;
+        if search_id(id, &ranges).is_some() {
+            part1 += 1;
         }
     }
 
-    println!("{total}");
+    for range in ranges {
+        part2 += range.1 + 1 - range.0;
+    }
+
+    println!("Part 1: {part1}");
+    println!("Part 2: {part2}");
 }
